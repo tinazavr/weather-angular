97b481c227575a3cffd316fcb3c4ac5e6aae5996
Anastasiia Zavorotynska <zavorotinskaya.ns@gmail.com>
2023-11-19 23:37:32 +0100

    Hide token to git ignored file.

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,12 +5,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class WeatherService {
-  apiKey = '76df6c935cecde1de08d21993b13cead';
-  constructor(private httpClient: HttpClient) {}
 
-  getInformation(city: string) {
-    this.httpClient.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`
-    );
-  }
 }
